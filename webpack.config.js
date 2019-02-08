85be32acd0c08dda44663ac31395273d604292f0
sabyasachi13 <sabyasachi.rout1@gmail.com>
2019-02-08 22:15:59 +0530

    error in dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const path =require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin =require('extract-text-webpack-plugin');
 
-process.env.NODE_ENV =process.env.NODE_ENV || 'development';
+process.env.NODE_ENV =process.env.NODE_ENV ||'development';
 if(process.env.NODE_ENV ==='test'){
     require ('dotenv').config({path : '.env.test'});
 }
 else if(process.env.NODE_ENV==='development'){
-    require ('dotenv').config({path : '.env.devlopment'});
+    require ('dotenv').config({path : '.env.development'});
 }
 
 module.exports =(env)=>{
@@ -52,7 +52,6 @@ module.exports =(env)=>{
                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-
             })
         ],
         devtool: isProduction? 'source-map':'inline-source-map',
